1caa405a9740bbf05bff03a292403dc601036c59
Ondra Tethal <tethal@email.cz>
2024-08-27 21:49:57 +0200

    Room code form, player WS connection management, set player name

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,9 @@
 from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
 from sqlmodel import Session, create_engine
 
-templates = Jinja2Templates(directory="templates")
+jinja_env = Environment(loader=FileSystemLoader("templates"))
+templates = Jinja2Templates(env=jinja_env)
 engine = create_engine("sqlite:///gamedify.db", echo=True, connect_args={"check_same_thread": False})
 
 
