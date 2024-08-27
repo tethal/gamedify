f23f6c89d3911da90f475b3510d36cce379cb003
Ondra Tethal <tethal@email.cz>
2024-08-27 12:16:14 +0200

    Remove usage of jinja2_fragments

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,7 @@
-from jinja2_fragments.fastapi import Jinja2Blocks
+from fastapi.templating import Jinja2Templates
 from sqlmodel import Session, create_engine
 
-templates = Jinja2Blocks(directory="templates")
+templates = Jinja2Templates(directory="templates")
 engine = create_engine("sqlite:///gamedify.db", echo=True, connect_args={"check_same_thread": False})
 
 
