9ec34ddcfd0f638d2425dffc2002507f224f2e24
davidk <davks11@gmail.com>
2024-12-13 17:11:11 +0100

    Add sandwich, favicon, minor adjustments

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+function scrollToSection(targetId) {
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
